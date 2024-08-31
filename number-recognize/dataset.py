e04abe23240675ff2051ae68a4880e20ea00853d
sunfishlk <2577095260@qq.com>
2024-08-31 10:59:18 +0800

    :zap: remove conda interpreter environment

--- number-recognize/dataset.py
+++ number-recognize/dataset.py
@@ -6,10 +6,10 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-# train_dataset = datasets.MNIST(root='./number-recognize/data/mnist', train=True,  transform=transform)  # vscode默认的根路径是根目录
-# test_dataset = datasets.MNIST(root='./number-recognize/data/mnist', train=False,  transform=transform)  # vscode默认的根路径是根目录
-train_dataset = datasets.MNIST(root='./data/mnist', train=True,  transform=transform)  # pycharm默认根路径是当前项目路径
-test_dataset = datasets.MNIST(root='./data/mnist', train=False,  transform=transform)  # pycharm默认根路径是当前项目路径
+train_dataset = datasets.MNIST(root='./number-recognize/data/mnist', train=True,  transform=transform)  # vscode默认的根路径是根目录
+test_dataset = datasets.MNIST(root='./number-recognize/data/mnist', train=False,  transform=transform)  # vscode默认的根路径是根目录
+# train_dataset = datasets.MNIST(root='./data/mnist', train=True,  transform=transform)  # pycharm默认根路径是当前项目路径
+# test_dataset = datasets.MNIST(root='./data/mnist', train=False,  transform=transform)  # pycharm默认根路径是当前项目路径
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
