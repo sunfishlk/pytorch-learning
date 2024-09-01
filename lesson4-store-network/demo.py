6d17dfcfeeee3f6a1afc1ad3e3ab5f2f486bcbab
sunfishlk <2577095260@qq.com>
2024-09-01 14:07:40 +0800

    :sparkles: store and restore network

---
+++ lesson4-store-network/demo.py
@@ -0,0 +1,73 @@
+import torch
+import matplotlib.pyplot as plt
+
+# fake data
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+y = x.pow(2) + 0.2*torch.rand(x.size())  # noisy y data (tensor), shape=(100, 1)
+
+def save():
+    net1 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+    optimizer = torch.optim.SGD(net1.parameters(), lr=0.5)
+    loss_func = torch.nn.MSELoss()
+
+    for t in range(100):
+        prediction = net1(x)
+        loss = loss_func(prediction, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # plot result
+    plt.figure(1, figsize=(10, 3))
+    plt.subplot(131)
+    plt.title('Net1')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+
+    # 2 ways to save the net
+    torch.save(net1, './lesson4-store-network/net.pkl')  # save entire net
+    torch.save(net1.state_dict(), './lesson4-store-network/net_params.pkl')   # save only the parameters
+
+# 保存整个网络，无需再建立网络
+def restore_net():
+    # restore entire net1 to net2
+    net2 = torch.load('./lesson4-store-network/net.pkl')
+    prediction = net2(x)
+
+    # plot result
+    plt.subplot(132)
+    plt.title('Net2')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+
+# 只保存网络参数，所以需要建立网络
+def restore_params():
+    # restore only the parameters in net1 to net3
+    net3 = torch.nn.Sequential(
+        torch.nn.Linear(1, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 1)
+    )
+    # copy net1's parameters into net3
+    net3.load_state_dict(torch.load('./lesson4-store-network/net_params.pkl'))
+    prediction = net3(x)
+
+    # plot result
+    plt.subplot(133)
+    plt.title('Net3')
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+    plt.show()
+
+# save net1
+save()
+
+# restore entire net (may slow)
+restore_net()
+
+# restore only the net parameters
+restore_params()
